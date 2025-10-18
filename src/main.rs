eb9417643282d9415a378f889767c386d9e75fdd
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-10-18 16:11:08 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            let line_offset: isize = 160 * 2;
+            let char_offset_within_line: isize = i as isize * 2;
+            let color_offset_within_line: isize = i as isize * 2 + 1;
+            let char_offset = char_offset_within_line + line_offset;
+            let color_offset = color_offset_within_line + line_offset;
+
+
+            *vga_buffer.offset(char_offset) = byte;
+            *vga_buffer.offset(color_offset) = 0xb;
+        }
+    }
+
+    loop {}
+}
