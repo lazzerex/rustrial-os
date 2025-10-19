19e35e81eae0922cc97f1fa7aa5594e02f429762
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-10-19 17:08:32 +0700

    Add QEMU exit via isa-debug-exit I/O port

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,37 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 
+#![reexport_test_harness_main = "test_main"]
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }
 
@@ -19,6 +45,20 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello from Rustrial Kernel!").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello from Rustrial Kernel{}", "!");
+
+    #[cfg(test)]
+    test_main();
+    //panic!("Crash and burn");
+
     loop {}
 }
 
@@ -45,16 +85,8 @@ fn panic(info: &PanicInfo) -> ! {
 //     loop {}
 // }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello from Rustrial Kernel!").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
-    println!("Hello from Rustrial Kernel{}", "!");
-    //panic!("Crash and burn");
 
-    loop {}
-}
+
 
 
