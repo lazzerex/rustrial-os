f53d0a19cb461ffe8196fa7d22f59a751af2f4db
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-10-20 17:29:11 +0700

    add testable trait

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,19 @@
 
 #![reexport_test_harness_main = "test_main"]
 
+
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) { 
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run(); 
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -35,6 +34,21 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
+    }
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
     }
 }
 
