c32f800b713cf3ee7f3255259c4c1494b009fd5b
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-10-26 23:36:28 +0700

    test mapping

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     //use x86_64::VirtAddr;
     //use rustrial_os::memory::translate_addr;
     use rustrial_os::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
-    
+    // use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::{structures::paging::Page, VirtAddr}; 
 
     println!("Hello From the Rustrial Kernel{}", "!");
    
@@ -76,20 +76,27 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // }
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
+    // let addresses = [
+    //     0xb8000,
+    //     0x201008,
+    //     0x0100_0020_1a10,
+    //     boot_info.physical_memory_offset,
+    // ];
+
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
 
     #[cfg(test)]
     test_main();
