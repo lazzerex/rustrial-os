4d2af9ffa4db226af56f919ece05542189a71332
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-10-27 00:01:41 +0700

    Implement BootInfoFrameAllocator using the bootloader memory map

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     //use rustrial_os::memory::translate_addr;
     use rustrial_os::memory;
     // use x86_64::{structures::paging::Translate, VirtAddr};
-    use x86_64::{structures::paging::Page, VirtAddr}; 
+    use x86_64::{structures::paging::Page, VirtAddr};
+    use rustrial_os::memory::BootInfoFrameAllocator; 
 
     println!("Hello From the Rustrial Kernel{}", "!");
    
@@ -77,13 +78,14 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let page = Page::containing_address(VirtAddr::new(0));
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
 
     // let addresses = [
     //     0xb8000,
