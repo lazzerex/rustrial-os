9d9fa938b41d3a210649e15585a4700f3bc8b17a
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-10-28 22:47:48 +0700

    update linked list allocator with GlobalAlloc

--- src/allocator.rs
+++ src/allocator.rs
@@ -2,6 +2,7 @@ pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
 
 pub mod bump;
+pub mod linked_list;
 
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
@@ -13,9 +14,11 @@ use x86_64::{
 };
 // use linked_list_allocator::LockedHeap;
 use bump::BumpAllocator;
+use linked_list::LinkedListAllocator;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> =
+    Locked::new(LinkedListAllocator::new());
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -31,10 +31,6 @@ fn main(boot_info: &'static BootInfo) -> ! {
     loop {}
 }
 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    rustrial_os::test_panic_handler(info)
-}
 
 #[test_case]
 fn simple_allocation() {
@@ -60,4 +56,19 @@ fn many_boxes() {
         let x = Box::new(i);
         assert_eq!(*x, i);
     }
+}
+
+#[test_case]
+fn many_boxes_long_lived() {
+    let long_lived = Box::new(1);
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+    assert_eq!(*long_lived, 1); 
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rustrial_os::test_panic_handler(info)
 }
