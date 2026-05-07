3ba69f819d6ffc42709647c570d056aca02334af
lazzerex <nambinh236@gmail.com>
2026-05-07 22:19:57 +0700

    feat: expand theme color options

--- src/theme.rs
+++ src/theme.rs
@@ -8,13 +8,43 @@ pub fn get() -> u8 {
 }
 
 pub fn set(t: u8) {
-    THEME_ID.store(t.min(2), Ordering::Relaxed);
+    THEME_ID.store(t.min(4), Ordering::Relaxed);
 }
 
 pub fn desktop_bg() -> Color {
     match get() {
         1 => Color::Black,
         2 => Color::Blue,
+        3 => Color::Green,
+        4 => Color::Magenta,
         _ => Color::Cyan,
+    }
+}
+
+pub fn title_bar_bg() -> Color {
+    match get() {
+        1 => Color::DarkGray,
+        2 => Color::Blue,
+        3 => Color::DarkGray,
+        4 => Color::Magenta,
+        _ => Color::Blue,
+    }
+}
+
+pub fn taskbar_bg() -> Color {
+    match get() {
+        1 => Color::Black,
+        2 => Color::Black,
+        3 => Color::DarkGray,
+        4 => Color::DarkGray,
+        _ => Color::DarkGray,
+    }
+}
+
+pub fn taskbar_fg() -> Color {
+    match get() {
+        3 => Color::LightGreen,
+        4 => Color::Pink,
+        _ => Color::LightGray,
     }
 }
